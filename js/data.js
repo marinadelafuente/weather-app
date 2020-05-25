b04c7e4505f4db92478221aa5f2e98cb695aa117
marinadelafuente <mdelafuente18@gmail.com>
2020-05-25 16:58:01 +0800

    added local storage

--- js/data.js
+++ js/data.js
@@ -1,23 +1,26 @@
-const key = "mCXTeoezstHPFNpHcGHKiGEGZknflmAG";
+const key = "52JG9KuwuniaPhjFHcDKqD8uzkEtAgzw";
 
 const getCurrentWeather = async (locationKey) => {
-    const response = await fetch(`https://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${key}`);
-    if (response.status !== 200) {
-        location.href = "../errorPage.html";
-        throw new Error('cannot fetch the data');
-    }
-    const data = await response.json();
+  const response = await fetch(
+    `https://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${key}`
+  );
+  if (response.status !== 200) {
+    location.href = "../errorPage.html";
+    throw new Error("cannot fetch the data");
+  }
+  const data = await response.json();
 
-    return data[0];
-}
+  return data[0];
+};
 
 const getCity = async (city) => {
-    const response = await fetch(`https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${key}&q=${city}`);
-    if (response.status !== 200) {
-        location.href = "../errorPage.html";
-        throw new Error('cannot fetch the data');
-
-    }
-    const data = await response.json();
-    return data[0];
+  const response = await fetch(
+    `https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${key}&q=${city}`
+  );
+  if (response.status !== 200) {
+    location.href = "../errorPage.html";
+    throw new Error("cannot fetch the data");
+  }
+  const data = await response.json();
+  return data[0];
 };
--- js/main.js
+++ js/main.js
@@ -1,113 +1,138 @@
-const body = document.querySelector('body');
-const search = document.querySelector('.js-form');
-const searchInput = document.querySelector('.js-input');
-const btn = document.querySelector('.js-btn');
-const card = document.querySelector('.js-card');
-const error = document.querySelector('.js-error');
-const img = document.querySelector('.js-img');
-const iconContainer = document.querySelector('.js-icon-container');
-const city = document.querySelector('.js-city');
-const weatherText = document.querySelector('.js-weather');
-const temp = document.querySelector('.js-temp');
-const loaderContainer = document.querySelector('.progress');
-const loader = document.querySelector('.progress-bar');
+const body = document.querySelector("body");
+const search = document.querySelector(".js-form");
+const searchInput = document.querySelector(".js-input");
+const btn = document.querySelector(".js-btn");
+const card = document.querySelector(".js-card");
+const error = document.querySelector(".js-error");
+const img = document.querySelector(".js-img");
+const iconContainer = document.querySelector(".js-icon-container");
+const city = document.querySelector(".js-city");
+const weatherText = document.querySelector(".js-weather");
+const temp = document.querySelector(".js-temp");
+const loaderContainer = document.querySelector(".progress");
+const loader = document.querySelector(".progress-bar");
 
+const getLocalStorage = () => {
+  if (localStorage.getItem("searchedCity")) {
+    searchedCity = localStorage.getItem("searchedCity");
+    getData();
+  }
+};
+
+const getData = () => {
+  getApiInfo(searchedCity)
+    .then((data) => {
+      updateInterface(data);
+    })
+    // if error
+    .catch((error) => {
+      loaderContainer.classList.add("d-none");
+      search.reset();
+
+      if (error.message === "Failed to fetch") {
+        location.href = "../errorPage.html";
+      } else {
+        alert("We cannot find that city. Please, try again.");
+        card.classList.add("d-none");
+        console.log(error.message);
+      }
+    });
+};
 
 // update UI with API data
 const updateInterface = (data) => {
-    //destructuring properties
-    const {
-        cityName,
-        weather
-    } = data;
+  // hide loader
+  loaderContainer.classList.add("d-none");
 
-    city.innerHTML = `${cityName.LocalizedName}, ${cityName.Country.LocalizedName}`
-    weatherText.innerHTML = weather.WeatherText;
-    temp.innerHTML = `${weather.Temperature.Metric.Value}&deg;C`;
+  //destructuring properties
+  const { cityName, weather } = data;
+  city.innerHTML = `${cityName.LocalizedName}, ${cityName.Country.LocalizedName}`;
+  weatherText.innerHTML = weather.WeatherText;
+  temp.innerHTML = `${weather.Temperature.Metric.Value}&deg;C`;
 
-    // hide/show the weather card info
-    if (card.classList.contains('d-none')) {
-        card.classList.remove('d-none')
-    }
+  // hide/show the weather card info
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
 
-    // hide loader
-    loaderContainer.classList.add('d-none');
+  // daytime/nightime
+  time = null;
+  if (weather.IsDayTime) {
+    time = "img/day.svg";
+    card.classList.replace("bg-night-mode", "bg-primary");
+    btn.classList.replace("btn-outline-primary", "btn-primary");
+    card.classList.remove("text-white");
+    iconContainer.classList.remove("icon-container__night");
+  } else {
+    time = "img/night.svg";
+    card.classList.replace("bg-primary", "bg-night-mode");
+    btn.classList.replace("btn-primary", "btn-outline-primary");
+    card.classList.add("text-white");
+    iconContainer.classList.add("icon-container__night");
+  }
+  img.setAttribute("src", time);
 
-    // daytime/nightime
-    time = null;
-    if (weather.IsDayTime) {
-        time = "img/day.svg"
-        card.classList.replace('bg-night-mode', 'bg-primary')
-        btn.classList.replace('btn-outline-primary', 'btn-primary')
-        card.classList.remove('text-white')
-        iconContainer.classList.remove('icon-container__night')
-
-    } else {
-        time = "img/night.svg"
-        card.classList.replace('bg-primary', 'bg-night-mode')
-        btn.classList.replace('btn-primary', 'btn-outline-primary')
-        card.classList.add('text-white')
-        iconContainer.classList.add('icon-container__night')
-    }
-    img.setAttribute('src', time)
-
-    // weather icons
-    const icons = `img/icon/${weather.WeatherIcon}.png`
-    iconContainer.innerHTML = `<img src=${icons} alt="${weather.WeatherText} icon" class="icon"></img>`
-}
+  // weather icons
+  const icons = `img/icon/${weather.WeatherIcon}.png`;
+  iconContainer.innerHTML = `<img src=${icons} alt="${weather.WeatherText} icon" class="icon"></img>`;
+};
 
 // get info from server
 const getApiInfo = async (city) => {
-    const cityName = await getCity(city);
-    const weather = await getCurrentWeather(cityName.Key)
+  const cityName = await getCity(city);
+  const weather = await getCurrentWeather(cityName.Key);
 
-    return {
-        cityName,
-        weather
-    };
-}
+  return {
+    cityName,
+    weather,
+  };
+};
 
 // search for city
 const handlerSearch = (ev) => {
-    ev.preventDefault();
+  ev.preventDefault();
 
-    // show loader while waiting for data 
-    loaderContainer.classList.remove('d-none');
+  // show loader while waiting for data
+  loaderContainer.classList.remove("d-none");
 
-    // get city value
-    const searchedCity = searchInput.value; // search.search.value
+  // get city value
+  let searchedCity = searchInput.value; // search.search.value
 
-    // show error message when empty
-    if (!searchedCity) {
-        if (error.classList.contains('d-none')) {
-            error.classList.remove('d-none')
-        }
-
-    } else {
-        // hide error message when empty
-        if (error.classList.contains('d-none') === false) {
-            error.classList.add('d-none')
-        }
-        // get info from the API matching the city
-        getApiInfo(searchedCity)
-            .then(data => {
-                console.log(data)
-                updateInterface(data);
-            })
-            // if error
-            .catch(error => {
-                loaderContainer.classList.add('d-none');
-                search.reset();
-
-                if (error.message === 'Failed to fetch') {
-                    location.href = "../errorPage.html"
-                } else {
-                    alert('We cannot find that city. Please, try again.')
-                    card.classList.add('d-none')
-                    console.log(error.message);
-                }
-            })
+  // show error message when empty
+  if (!searchedCity) {
+    if (error.classList.contains("d-none")) {
+      error.classList.remove("d-none");
     }
-}
+  } else {
+    // hide error message when empty
+    if (error.classList.contains("d-none") === false) {
+      error.classList.add("d-none");
+    }
+    //get info from the API matching the city
+    getApiInfo(searchedCity)
+      .then((data) => {
+        console.log(data);
+        updateInterface(data);
+      })
+      // if error
+      .catch((error) => {
+        loaderContainer.classList.add("d-none");
+        search.reset();
 
-search.addEventListener('submit', handlerSearch);
+        if (error.message === "Failed to fetch") {
+          location.href = "../errorPage.html";
+        } else {
+          alert("We cannot find that city. Please, try again.");
+          card.classList.add("d-none");
+          console.log(error.message);
+        }
+      });
+  }
+  // set local storage
+  localStorage.setItem("searchedCity", searchedCity);
+};
+
+
+search.addEventListener("submit", handlerSearch);
+
+getLocalStorage();
